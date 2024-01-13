97eddc564a0d68a11e69ab755fe1f77bc0a60dc2
code0em <146350167+Code0Em@users.noreply.github.com>
2024-01-13 14:31:14 +0000

    completed task 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,12 +17,49 @@
 
 // **GLOBAL VARIABLES**
 
-// TASK 1: Api Key
+// TASK 1: Api Key.
 const apiKey = "0da7a739bbb63431efc4b479cf47b78e";
 
-// !TO BE MOVED
-// TASK 1: Query URL for the API (returning user's input)
-const userQueryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${userSearch}&units=metric&appid=${apiKey}`
+// Gets references for all of the HTML elements that we need.
+const searchBtn = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+const errorMessage = document.getElementById("error");
 
-// TASK 1: Query URL for the API (returning saved city)
+// !TO BE MOVED
+// TASK 1: Query URL for the API (returning saved city).
 const historyQueryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${savedCity}&units=metric&appid=${apiKey}`
+
+// **EVENT LISTENERS**
+// TASK 2: Listens for a click event on the search button and calls function.
+searchBtn.addEventListener("click", function (e) {
+    // Prevents the default behaviour (i.e. reloading the page).
+    e.preventDefault();
+    // Gets what the user's inputted into the search input (i.e. the value of input element).
+    const userSearch = searchInput.value;
+    // Validates the search input by checking it's not empty (i.e. if the length of the value is zero, run this codeblock).
+    if (userSearch.length == 0) {
+        // Displays error message (i.e. sets the inner HTML of the p element).
+        errorMessage.innerHTML = "Please enter a city."
+        // If the search input isn't empty, run this codeblock:
+    } else {
+        // Checks if there's an error message already displayed (i.e. if the inner HTML of p doesn't equal zero, run this codeblock).
+        if (errorMessage.innerHTML.length !== 0) {
+            // Removes the existing error message (i.e. sets inner HTML of p to empty string).
+            errorMessage.innerHTML = " ";
+        } // Continues through the function
+
+        // TASK 1: Query URL for the API (returning user's input).
+        const userQueryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${userSearch}&units=metric&appid=${apiKey}`
+        // Runs the fetch method on the API query URL.
+        fetch(userQueryUrl)
+            .then(function (response) {
+                // Formats the returned data into a usable form, using json method.
+                return response.json();
+            })
+            // Waits for the data to be formatted.
+            .then(function (data) {
+                // Calls the function to display today's forecast.
+                createTodaysForecast(data);
+            });
+    }
+});
